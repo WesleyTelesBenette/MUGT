df6b1749d2f3014b359d20467696a6e2686947c6
Wesley T. Benette <weluonf@gmail.com>
2024-03-14 13:26:37 -0300

    Separando funcionalidades em componentes; adicionando SVGs dos links.

---
+++ src/app/components/attributes/attributes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attributes',
+  standalone: true,
+  imports: [],
+  templateUrl: './attributes.component.html',
+  styleUrl: './attributes.component.scss'
+})
+export class AttributesComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { LogoComponent } from '../../components/logo/logo.component';
+import { AttributesComponent } from '../../components/attributes/attributes.component';
+import { LinksComponent } from '../../components/links/links.component';
+import { ButtonsComponent } from '../../components/buttons/buttons.component';
 
 @Component({
-    selector: 'app-home',
-    standalone: true,
-    imports: [LogoComponent],
-    templateUrl: './home.component.html',
-    styleUrl: './home.component.scss'
+  selector: 'app-home',
+  standalone: true,
+  imports: [LogoComponent, AttributesComponent, LinksComponent, ButtonsComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
-export class HomeComponent
-{
-    generatedClick(): void {
-        
-    }
+export class HomeComponent {
+  generatedClick(): void {
+
+  }
 
 }
