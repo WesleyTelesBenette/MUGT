1af4ec3f2eff4a083766e8453e08a40ee26ee60b
Wesley T. Benette <weluonf@gmail.com>
2024-03-14 17:31:13 -0300

    Implementando a distribuição dinâmica de atributos.

--- src/app/components/attributes/attributes.component.ts
+++ src/app/components/attributes/attributes.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-attributes',
-  standalone: true,
-  imports: [],
-  templateUrl: './attributes.component.html',
-  styleUrl: './attributes.component.scss'
+    selector: 'app-attributes',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './attributes.component.html',
+    styleUrl: './attributes.component.scss'
 })
 export class AttributesComponent {
+    attributes: string[] = ["Nome", "CPF", "RG", "Idade", "Gênero", "Macumba"];
+
 
 }
