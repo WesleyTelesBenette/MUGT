06fa8f44de522d9db7fc5674248e5297897bf777
Wesley T. Benette <weluonf@gmail.com>
2024-03-14 21:07:44 -0300

    Importando Bootstrap; Implementação completa dos links do Header.

--- src/app/components/links/links.component.ts
+++ src/app/components/links/links.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-links',
-  standalone: true,
-  imports: [],
-  templateUrl: './links.component.html',
-  styleUrl: './links.component.scss'
+    selector: 'app-links',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './links.component.html',
+    styleUrl: './links.component.scss'
 })
 export class LinksComponent {
+    public currentCopied: string = "URL";
 
+    public url: string = "Ainda não tem...";
+    public route: string = "/generated/user";
+
+    constructor(private cdr: ChangeDetectorRef) { }
+
+    private copyToClipBoard(text: string): boolean {
+        const toast = document.getElementById('toast');
+
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                toast?.classList.add('toast-on');
+                this.cdr.detectChanges();
+                setTimeout(() => {
+                    toast?.classList.remove('toast-on')
+                }, 6000);
+            })
+            .catch(err => {
+                console.error('Erro ao copiar texto para a área de transferência: ', err);
+                return false;
+            });
+        return toast?.classList.contains('toast-on').valueOf != null;
+    }
+
+    public copyUrl(): void {
+        this.currentCopied = "URL";
+        this.copyToClipBoard(this.url);
+    }
+
+    public copyRoute(): void {
+        this.currentCopied = "ROTA";
+        this.copyToClipBoard(this.route);
+    }
 }
